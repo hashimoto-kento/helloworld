49326e8a4fc77994c7254dabf12fed66261a26f0
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-10-10 07:36:51 +0900

    count up, count down

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <Counter />;
+};
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+  }
+
+  onIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  onDecrement = () => {
+    this.setState({ value: this.state.value - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>カウント値：{this.state.value}</div>
+        <div>
+          <button onClick={this.onIncrement}>+</button>
+          <button onClick={this.onDecrement}>-</button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
